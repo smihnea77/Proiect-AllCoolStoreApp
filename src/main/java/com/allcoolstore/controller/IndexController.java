fe7c9879d351891e587c250b0a8e7140f09cc6ad
DavidS98 <davidsecheres1998@gmail.com>
2022-12-06 13:47:05 +0200

    logout

--- src/main/java/com/allcoolstore/controller/IndexController.java
+++ src/main/java/com/allcoolstore/controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.allcoolstore.controller;
 
 import com.allcoolstore.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class IndexController {
@@ -72,10 +75,16 @@ public class IndexController {
     public String getNotRegisterPage() {
         return "notRegister";
     }
-    @GetMapping(value = "logout")
-    public String logoutPage(){
-        return "logout";
+
+    @GetMapping(value = "/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
+
 
 
 }
